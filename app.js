351c689934837a35150e75876e9c263a8e178657
Hemin Johnson <hemin.johnson@mieterengel.de>
2018-10-07 15:43:21 +0200

    feat: handle callback error

--- app.js
+++ app.js
@@ -11,13 +11,20 @@ const argv = yargs.option({
 }).help().alias('help', 'h').argv
 
 var encodedAddress = encodeURIComponent(argv.address)
+console.log(encodedAddress)
 
 request({
   //add the right key or the code doesnot work
   url: `http://www.mapquestapi.com/geocoding/v1/address?key=KEY&location=${encodedAddress}`,
   json: true
 }, (err, res, body) => {
-  console.log(`Address: ${body.results[0].providedLocation.location}`)
-  console.log(`Latitude: ${body.results[0].locations[0].latLng.lat}`)
-  console.log(`Longitude: ${body.results[0].locations[0].latLng.lng}`)
+  if(err) {
+    console.log('unable to connect to the servers')
+  } else if (body.info.statuscode === 400) {
+    console.log('please provide an address')
+  } else if (body.info.statuscode === 0) {
+    console.log(`Address: ${body.results[0].providedLocation.location}`)
+    console.log(`Latitude: ${body.results[0].locations[0].latLng.lat}`)
+    console.log(`Longitude: ${body.results[0].locations[0].latLng.lng}`)
+  }
 })
