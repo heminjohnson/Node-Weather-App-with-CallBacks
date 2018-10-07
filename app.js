4c791d299fc48174795d17141f355b5e1331a2c7
Hemin Johnson <hemin.johnson@mieterengel.de>
2018-10-07 14:12:05 +0200

    feat: add user input feature and encode user input

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const request = require('request')
+const yargs = require('yargs')
+
+const argv = yargs.option({
+  a: {
+    demand: true,
+    alias: 'address',
+    describe: 'Address to fetch the weather',
+    string: true
+  }
+}).help().alias('help', 'h').argv
+
+var encodedAddress = encodeURIComponent(argv.address)
 
 request({
   //add the right key or the code doesnot work
-  url: 'http://www.mapquestapi.com/geocoding/v1/address?key=KEY&location=1301%20lombard%20street%20philadelphia',
+  url: `http://www.mapquestapi.com/geocoding/v1/address?key=KEY&location=${encodedAddress}`,
   json: true
 }, (err, res, body) => {
   console.log(`Address: ${body.results[0].providedLocation.location}`)
